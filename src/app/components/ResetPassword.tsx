95dcc0131495ddf072fbdb4fb8379e1f545c94b9
Victor Anya <anyovic1007@gmail.com>
2026-05-03 15:24:22 +0100

    Update import path for supabase client

--- src/app/components/ResetPassword.tsx
+++ src/app/components/ResetPassword.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from "../../lib/supabase"
+import { supabase } from "../../lib/supabaseClient.ts"
 
 export function ResetPassword() {
   const navigate = useNavigate();
